d9cd76609635f598ca198de3142fd549de3cb5ac
thdaemon <thxdaemon@gmail.com>
2017-07-18 18:52:25 +0800

    Update gh-wikitool/wikigen.sh

--- gh-wikitool/wikigen.sh
+++ gh-wikitool/wikigen.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+gen_md()
+{
+	#local filename=${2}/${1##*/}
+	local tmp=`head -1 "$1"`
+	tmp=${tmp#* }
+	local filename=${2}/${tmp// /-}.md
+	cat > $filename << EOF
+**This file is generated from ${i} by script, you can directly access the original version in source code directory**
+
+Generate time (UTC): `date -R -u`
+
+EOF
+	cat "$1" >> "$filename"
+}
+
 clonefrom=https://github.com/thdaemon/mydm.wiki.git
 dirname=mydm.wiki
 
@@ -11,14 +26,14 @@ fi
 outdir=build_wiki/${dirname}
 
 for i in `find doc/ -type f`; do
-	filename=${outdir}/${i##*/}
-	cat > $filename << EOF
-**This file is generated from ${i} by script, you can directly access the original version in source code dir**
-
-Generate time (UTC): `date -R -u`
-
-EOF
-	cat $i >> $filename
+	case ${i##*.} in
+	md|markdown)
+		gen_md "$i" "$outdir"
+		;;
+	*)
+		echo "Unknow file type $i" >&2
+		;;
+	esac
 done
 
 (cd ${outdir}; git add .; git commit -m 'It is commited by wikigen.sh'; git push --all origin)
